68563419be7964567f1a569120c7d8663bc6a3ee
Nick Leon <nicolas7@vt.edu>
2022-01-16 10:51:32 -0500

    file structure commit

---
+++ graph-buddy/src/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+
+
+
+function App() {
+
+  
+
+  return (
+    <div>
+        <h1>
+            Hello cruel world
+        </h1>
+    </div>
+
+  )
+}
+
+export default App;
