69e464b8fa66f055d67a2eb40d584795386700b1
Nick Leon <nicolas7@vt.edu>
2022-02-26 14:22:16 -0500

    reset

--- server/server.js
+++ server/server.js
@@ -1,35 +1,14 @@
 const path = require('path');
 const express = require('express');
-// const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
-
-// const { typeDefs, resolvers } = require('./schemas');
-// const { authMiddleware } = require('./utils/auth');
-
-// const db = require('./config/connection');
-
-const PORT = process.env.PORT || 3001;
 const app = express();
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   context: authMiddleware,
-// });
-
-// server.applyMiddleware({ app });
-
-
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
-
+const publicPath = path.join(__dirname, '../graph-buddy/', 'public');
 const publicPath = path.join(__dirname, '../graph-buddy/', 'build/');
+const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
-app.listen(PORT, () => {
+app.listen(port, () => {
    console.log('Server is up!');
 });
 
