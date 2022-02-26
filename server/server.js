77b469151782dad300f3725071bb58decabb9dfb
Nick Leon <nicolas7@vt.edu>
2022-02-26 14:40:46 -0500

    import apollo

--- server/server.js
+++ server/server.js
@@ -3,6 +3,24 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '../graph-buddy/', 'build/');
 const port = process.env.PORT || 3000;
+
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
