3c4f138532ddb156cce32a5bddd4bc60b3de4aed
Nick Leon <nicolas7@vt.edu>
2022-02-26 13:43:14 -0500

    no db connect

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const path = require('path');
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
@@ -11,26 +17,21 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-const db = require('./config/connection');
-
-const app = express();
 server.applyMiddleware({ app });
+
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
 const publicPath = path.join(__dirname, '../graph-buddy/', 'build/');
-const port = process.env.PORT || 3001;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
-
-db.once('open', () => {
-  app.listen(port, () => {
-     console.log('Server is up!');
-  });
-})
+app.listen(port, () => {
+   console.log('Server is up!');
+});
 
 // const express = require('express');
 // const { ApolloServer } = require('apollo-server-express');
