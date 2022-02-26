21b1da907bcfa4040e0155ac4e6d7622d1179b3a
Nick Leon <nicolas7@vt.edu>
2022-02-26 13:52:58 -0500

    remove middleware

--- server/server.js
+++ server/server.js
@@ -17,11 +17,11 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-server.applyMiddleware({ app });
+// server.applyMiddleware({ app });
 
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 
 const publicPath = path.join(__dirname, '../graph-buddy/', 'build/');
