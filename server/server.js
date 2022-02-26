0432b0000b4b2e00f6fd832037936d090c01eba9
Nick Leon <nicolas7@vt.edu>
2022-02-26 14:55:44 -0500

    require apollo

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const publicPath = path.join(__dirname, '../graph-buddy/', 'build/');
 const port = process.env.PORT || 3000;
 
-// const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 // const { typeDefs, resolvers } = require('./schemas');
 // const { authMiddleware } = require('./utils/auth');
 // const db = require('./config/connection');
