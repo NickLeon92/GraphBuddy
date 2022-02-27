388bf76244e24a0f0cd16d73150acc604c418394
Nick Leon <nicolas7@vt.edu>
2022-02-27 13:05:37 -0500

    reset

--- server/server.js
+++ server/server.js
@@ -1,73 +1,36 @@
-const path = require('path');
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
 const { ApolloServer } = require('apollo-server-express');
-const { signToken } = require('./utils/auth');
-// const { Schema, model } = require('mongoose');
-// const bcrypt = require('bcrypt');
-// const { typeDefs, resolvers } = require('./schemas');
-const typeDefs = require('./schemas/typeDefs')
-// const resolvers = require('./schemas/resolvers')
-const { authMiddleware } = require('./utils/auth');
+const path = require('path');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-// const server = new ApolloServer({
-  //   typeDefs,
-  //   resolvers,
-  //   context: authMiddleware,
-  // });
-  
-  // server.applyMiddleware({ app });
-  
-  // app.use(express.urlencoded({ extended: false }));
-  // app.use(express.json());
+const { authMiddleware } = require('./utils/auth');
 
-  
-const publicPath = path.join(__dirname, '../graph-buddy/', 'build/');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static(publicPath));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // Add context to our server so data from the `authMiddleware()` function can pass data to our resolver functions
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
-app.listen(port, () => {
-   console.log('Server is up!');
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// const express = require('express');
-// const { ApolloServer } = require('apollo-server-express');
-// const path = require('path');
 
-// const { typeDefs, resolvers } = require('./schemas');
-// const { authMiddleware } = require('./utils/auth');
-
-// const db = require('./config/connection');
-
-// const PORT = process.env.PORT || 3001;
-// const app = express();
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   context: authMiddleware,
-// });
-
-// server.applyMiddleware({ app });
-
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../graph-buddy/build')));
-// }
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../graph-buddy/build/index.html'));
-// });
-
-// db.once('open', () => {
-//   app.listen(PORT, () => {
-//     console.log(`API server running on port ${PORT}!`);
-//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-//   });
-// });
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+});
