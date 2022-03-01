a92d0f8f4462896f292bea8cceb4e2f364068c37
Nick Leon <nicolas7@vt.edu>
2022-03-01 16:36:50 -0500

    remove server var from console log

--- server/server.js
+++ server/server.js
@@ -36,5 +36,4 @@ app.get('*', (req, res) => {
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
